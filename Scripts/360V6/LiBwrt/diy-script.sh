2cd03b0407b1e777deb1950b10a966cda5f99b93
wwz09 <wwz5124@gmail.com>
2025-06-01 11:37:30 +0800

    Update diy-script.sh

--- Scripts/360V6/LiBwrt/diy-script.sh
+++ Scripts/360V6/LiBwrt/diy-script.sh
@@ -130,6 +130,8 @@ sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-set
 # 修复 hostapd 报错
 cp -f $GITHUB_WORKSPACE/scripts/360V6/LiBwrt/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
 
+cp -f $GITHUB_WORKSPACE/Scripts/patch/001-fix-ci-llvm-config.patch package/feeds/packages/rust/patches/001-fix-ci-llvm-config.patch
+
 # 修复 armv8 设备 xfsprogs 报错
 sed -i 's/TARGET_CFLAGS.*/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g' feeds/packages/utils/xfsprogs/Makefile
 
